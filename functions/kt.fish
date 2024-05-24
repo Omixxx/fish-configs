3d41e8c6be4994cc95789c0e66b7e911d2a6bdfc
Omixxx <xxcortex99@gmail.com>
2024-05-24 17:26:08 +0200

    update

--- functions/kt.fish
+++ functions/kt.fish
@@ -1,3 +1,3 @@
-function kt --wraps='kill ' --description 'alias kt=kill '
+function kt --wraps='kill ' --description 'alias kt=kill $(pidof telegram-desktop)'
   kill $(pidof telegram-desktop)  $argv
 end
